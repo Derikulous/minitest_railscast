72b9cc92427c17f4e3aaf82c1c1d7521af2beb33
Derik Strattan <derik.strattan@gmail.com>
2013-10-09 15:17:55 -0700

    The railscast for Minitest. #327

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '9181b473c040a96978788315465490d6a404a96c8feb94d61af9dd6f59ccc48a1d98f5fe65e218e39499c7146ad379c2e5ea68734813984332f3a6cba0600f3f'
